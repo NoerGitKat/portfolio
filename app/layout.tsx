803424bc164fd0572e70012ff95f190e0dce3cb2
Noer Paanakker <noerpaanakker@gmail.com>
2023-08-16 17:42:31 +0200

    Added starter project

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import { Sidebar } from "./components/sidebar";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Noer's Personal Website",
+  description:
+    "Noer is a business-savvy educationalist with technical skills. He has spent the last 6 years helping underrepresented groups into high-quality tech jobs. You can often find him nerding out on cool ideas, in an effort to leave the world a better place than he found it."
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>
+        <Sidebar />
+        {children}
+      </body>
+    </html>
+  );
+}
